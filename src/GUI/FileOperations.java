1352e945698effb8f84a73b85fe9fd209e5d37a1
DESKTOP-66UEMMG\Mohamed Kandeel <m.kandeel2277@gmail.com>
2022-12-13 19:36:50 +0200

    Project launch

---
+++ src/GUI/FileOperations.java
@@ -0,0 +1,80 @@
+package GUI;
+
+import Users.Admin;
+import Users.Employee;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileOperations {
+    public static ArrayList<Object> read(String FilePath) {
+
+        Scanner Reader = null;
+        try {
+            Reader = new Scanner(new File(FilePath));
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        }
+
+        if (FilePath.equals("admin.txt")) {
+
+            ArrayList<Admin> admins = new ArrayList<Admin>();
+            Admin x;
+
+            while (Reader.hasNext()) {
+
+                x = new Admin();
+                String Line = Reader.nextLine();
+                String[] seprated = Line.split(",");
+
+                x.setUsername(seprated[0]);
+                x.setPassword(seprated[1]);
+                admins.add(x);
+            }
+
+            return (ArrayList<Object>) (Object) admins;
+
+
+        }
+        else if (FilePath.equals("employee.txt")) {
+
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            Employee x;
+
+            while (Reader.hasNext()) {
+
+                x = new Employee();
+                String Line = Reader.nextLine();
+                String[] seprated = Line.split(",");
+
+                x.setID(Integer.parseInt(seprated[0]));
+                x.setUsername(seprated[1]);
+                x.setPassword(seprated[2]);
+                employees.add(x);
+            }
+
+            return (ArrayList<Object>) (Object) employees;
+
+
+        }
+        else
+            return null;
+    }
+
+    public static boolean write(String Query, String FilePath, boolean appendType) {
+
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter(new FileWriter(new File(FilePath), appendType));
+            writer.println(Query);
+            return true;
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            writer.close();
+        }
+        return false;
+    }
+}
---
+++ src/Users/Employee.java
@@ -0,0 +1,14 @@
+package Users;
+
+public class Employee extends User {
+
+    private int ID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+}
